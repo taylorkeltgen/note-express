db9ce8b497b61fc4aac66c86d8c3296656f33460
taylorkeltgen <taylorkeltgen@gmail.com>
2022-02-10 12:38:33 -0600

    server and routes setup

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
+router.post('/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
+
+module.exports = router;
